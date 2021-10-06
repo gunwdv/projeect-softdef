0a3b91ad0be48365cbfa10ac67a1800333921fa6
naphaporn <janjisung.shop@gmail.com>
2021-10-06 16:14:02 +0700

    soft-dev

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Applogin from './Applogin.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Applogin />
+    </div>
+  );
+}
+
+export default App;
